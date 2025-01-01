745e4ef796c06ac8dd4c11f53c2ffc852abc9f8c
Connor Greene <connor@connorgreene.net>
2025-01-01 14:42:29 -0500

    Set timezone to the One True zone for API endpoints

--- web/api/index.php
+++ web/api/index.php
@@ -5,6 +5,8 @@ $db_username = '';
 $db_password = '';
 $db_name = '';
 
+date_default_timezone_set('America/New_York');
+
 $conn = new mysqli($db_host, $db_username, $db_password, $db_name);
 
 if($conn->connect_error)
